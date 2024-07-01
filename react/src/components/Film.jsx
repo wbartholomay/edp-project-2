1da4b9f5185981c2cfc31be256f5150b708506ae
Bartholomay <alice@webage.com>
2024-07-01 15:42:50 -0400

    updated Film.jsx

--- react/src/components/Film.jsx
+++ react/src/components/Film.jsx
@@ -1,52 +1,62 @@
 import React, { useState, useEffect } from "react";
+import '../site.css'
 
 const Film = (props) => {
-  const filmId = props.filmId;
+  const filmId = props.id;
   const [data, setData] = useState();
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (url, setFunc) => {
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_SOCKS_API_URL}/films/${filmId}`
+          url
         );
         if (!response.ok) {
           throw new Error("Data could not be fetched!");
         }
         const json_response = await response.json();
-        setData(json_response);
+        setFunc(json_response);
+        console.log(json_response);
       } catch (error) {
-        console.error("Error fetching socks:", error);
+        console.error("Error fetching:", error);
       }
     };
-    fetchData();
+    const apiUrl = 'http://localhost:3000/api';
+    fetchData(`${apiUrl}/films/${filmId}`, setData);
+    fetchData(`${apiUrl}/films/${filmId}/characters`, setCharacters);
+    fetchData(`${apiUrl}/films/${filmId}/planets`, setPlanets);
   }, []);
   return (
+    <>
+    {data && characters && planets && (
     <>
       <h1 id="name"></h1>
       <section id="generalInfo">
         <p>
-          producer: {data.producer}<span id="producer"></span>
+          Producer: {data.producer}<span id="producer"></span>
         </p>
         <p>
-          title: {data.title}<span id="title"></span>
+          Title: {data.title}<span id="title"></span>
         </p>
         <p>
-          episode ID: {data.episode_id}<span id="episode_id"></span>
+          Episode ID: {data.episode_id}
         </p>
         <p>
-          director: {data.director}<span id="director"></span>
+          Director: {data.director}<span id="director"></span>
         </p>
         <p>
-          release Date: {data.release_data}<span id="release_date"></span>
+          Release Date: {data.release_date}<span id="release_date"></span>
         </p>
         <p>
-          opening crawl: {data.opening_crawl}<spam id="OpeningCrawl"></spam>
+          Opening Crawl: {data.opening_crawl}<span id="OpeningCrawl"></span>
         </p>
       </section>
       <h2>Character List</h2>
       <section id="charactersList"></section>
       <h2>Planet</h2>
       <section id="planetList"></section>
+      </>)}
     </>
   );
 };
