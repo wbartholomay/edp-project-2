b5c0a39cd617acf87dc8132e34f10f84f2b095b4
Bartholomay <alice@webage.com>
2024-07-01 13:47:49 -0400

    consolidated to 1 function

--- server/server.js
+++ server/server.js
@@ -26,32 +26,15 @@ app.get("/api/films/:id/characters", async (req, res) => {
   try {
     const { id } = req.params;
 
-    const collectionName = "films_characters";
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    const result = await findFromTwoDBs(
+      "films_characters",
+      "characters",
+      id,
+      "film_id",
+      "character_id"
+    );
 
-    const cursor = await collection
-      .find({ film_id: Number(id) });
-    //   .project({ character_id: 1, _id: 0 });
-
-    const characters = [];
-    const character_collection = db.collection("characters");
-
-    let i = 0;
-
-    while (await cursor.hasNext()) {
-        const obj = await cursor.next();
-        const id = obj.character_id;
-      const charCursor = await character_collection.find({ id: id });
-      if (await charCursor.hasNext()) {
-        const character = await charCursor.next();
-        characters.push(character);
-      }
-    }
-
-
-    res.json(characters);
+    res.json(result);
   } catch (err) {
     console.error("Error:", err);
     res.status(500).send("error");
@@ -59,40 +42,71 @@ app.get("/api/films/:id/characters", async (req, res) => {
 });
 
 app.get("/api/films/:id/planets", async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const collectionName = "films_planets";
-      const client = await MongoClient.connect(url);
-      const db = client.db(dbName);
-      const collection = db.collection(collectionName);
-  
-      const cursor = await collection
-        .find({ film_id: Number(id) });
-      //   .project({ character_id: 1, _id: 0 });
-  
-      const planets = [];
-      const planet_collection = db.collection("planets");
-  
-      let i = 0;
-  
-      while (await cursor.hasNext()) {
-          const obj = await cursor.next();
-          const id = obj.planet_id;
-        const charCursor = await planet_collection.find({ id: id });
-        if (await charCursor.hasNext()) {
-          const planet = await charCursor.next();
-          planets.push(planet);
-        }
-      }
-  
-  
-      res.json(planets);
-    } catch (err) {
-      console.error("Error:", err);
-      res.status(500).send("error");
+  try {
+    const { id } = req.params;
+    const result = await findFromTwoDBs(
+      "films_planets",
+      "planets",
+      id,
+      "planet_id",
+      "planet_id"
+    );
+
+    res.json(result);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("error");
+  }
+});
+
+app.get("/api/characters/:id/films", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await findFromTwoDBs(
+      "films_characters",
+      "films",
+      id,
+      "character_id",
+      "film_id"
+    );
+    res.json(result);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("error");
+  }
+});
+
+async function findFromTwoDBs(
+  collection_name1,
+  collection_name2,
+  params_id,
+  id1,
+  id2
+) {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection1 = db.collection(collection_name1);
+
+  const query = {};
+  query[id1] = Number(params_id);
+
+  const cursor = await collection1.find(query);
+  //   .project({ character_id: 1, _id: 0 });
+
+  const collection2 = db.collection(collection_name2);
+  const films = [];
+  while (await cursor.hasNext()) {
+    const obj = await cursor.next();
+    const id = obj[id2];
+    const charCursor = await collection2.find({ id: id });
+    if (await charCursor.hasNext()) {
+      const film = await charCursor.next();
+      films.push(film);
     }
-  });
+  }
+  return films;
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
