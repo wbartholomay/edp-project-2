74e626271619f49b05e010c67358983a9c4ea30d
Brandon Onyejekwe <bonyejek@travelers.com>
2024-07-01 16:18:49 -0400

    Display character info using cards

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react'
+
+import React, { useState, useEffect } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Character from "./components/Characters";
+import character_data from "./assets/characters.json";
+// import promo_data from "./assets/promo.json";
+// import Footer from "./components/Footer";
+// import Search from "./components/Search";
+// import Promotion from "./components/Promotion";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react/src/components/Characters.jsx
+++ react/src/components/Characters.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-const Character = () => {
+const Character = (props) => {
     return (
-        <div>
-            <h1>This is the Character component!</h1>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Name: {props.data.name}</h5>
+          <div className="card-text">Gender: {props.data.gender}</div>
+          <div className="card-text">Skin Color: {props.data.skin_color}</div>
+          <div className="card-text">Hair Color: {props.data.hair_color}</div>
+          <div className="card-text">Height: {props.data.height} </div>
+          <div className="card-text">Eye Color: {props.data.eye_color}</div>
+          <div className="card-text">Mass: {props.data.mass}</div>
+          <div className="card-text">Homeworld: {props.data.homeworld}</div>
+          <div className="card-text">Birth Year: {props.data.birth_year}</div>
         </div>
+        <div
+          className="card-footer"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <small className="text-muted">
+            Added: {props.data.addedTimestamp}
+          </small>
+          <button
+            className="btn btn-sm btn-danger"
+            onClick={() => props.handleDelete(props.data._id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     );
 };
 
